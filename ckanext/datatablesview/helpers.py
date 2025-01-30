5eb5eac640f384de6582801288de5e63cf52cfe2
unknown <owais.khan@360factors.com>
2025-01-30 07:40:39 +0100

    Backend service flow

--- ckanext/datatablesview/helpers.py
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-from ckan.plugins.toolkit import _, config
-
-
-def datatablesview_null_label():
-    label = config.get('ckan.datatables.null_label')
-    return _(label) if label else ''
